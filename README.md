d55e1f926ae00811e23288b7ca8563fc37421759
chilijung <chilijung@gmail.com>
2014-01-23 14:26:10 +0800

    init

--- README.md
+++ README.md
@@ -8,6 +8,29 @@ Auto detect different type of file format and convert it into JSON.
 - xlsx
 - xls
 
+## Sample
+
+```
+var cv_json = require('..');
+
+cv_json({
+    // now supporting csv, xls, xlsx format
+    input: __dirname + '/number_format.xls',
+    output: null
+  }, function(err, result) {
+    if(err) {
+      console.error(err);
+    }else {
+      console.log(result);
+    }
+  
+  }
+  );
+```
+
+In config object, you have to enter an input path. But If you don't want to output any file you can set to `null`.
+
+
 ## License
 
 MIT [@chiljung](http://github.com/chilijung)
---
+++ libs/index.js
@@ -0,0 +1,80 @@
+var fs = require('fs');
+var csv_json = require('node-csv-json');
+var xls_json = require('xls-to-json');
+var xlsx_json = require('xlsx-to-json');
+
+var header = []
+	, record = [];
+
+module.exports = CV_json;
+
+function CV_json (config, callback) {
+
+	if(!config.input) {
+		console.error("You have to have an input file");
+		process.exit(1);
+	}
+
+	var cv = new CV(config, callback);
+}
+
+CV = function(config, callback) {
+	var exten = this._getExtension(config.input);
+	switch(exten) {
+		case 'csv': 
+			return this.cvCSV(config, callback);
+			break;
+		case 'xls':
+			return this.cvXLS(config, callback);
+			break;
+		case 'xlsx':
+			return this.cvXLSX(config, callback);
+			break;
+		default:
+			callback('Not Support');
+	}
+}
+
+CV.prototype._getExtension = function(filename) {
+	return filename.split('.').pop();
+}
+
+CV.prototype.cvCSV = function(config, callback) {
+	csv_json({
+		input: config.input, 
+		output: config.output
+	}, function(err, result){
+		if(err) {
+			callback(err)
+		}else {
+			callback(null, result)
+		}
+	});
+	
+}
+
+CV.prototype.cvXLS = function(config, callback) {
+	xls_json({
+		input: config.input, 
+		output: config.output
+	}, function(err, result){
+		if(err) {
+			callback(err)
+		}else {
+			callback(null, result)
+		}
+	});
+}
+
+CV.prototype.cvXLSX = function(config, callback) {
+	xlsx_json({
+		input: config.input, 
+		output: config.output
+	}, function(err, result){
+		if(err) {
+			callback(err)
+		}else {
+			callback(null, result)
+		}
+	});
+}
